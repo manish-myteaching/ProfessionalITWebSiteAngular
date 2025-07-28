b423b06f01e650256548aa9b1d902e0db548db2e
manish-myteaching <manishverma.myteaching@gmail.com>
2025-07-29 02:36:48 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,17 +4,11 @@ export default {
   inlineCriticalCss: true,
   baseHref: '/ProfessionalITWebSiteAngular/',
   locale: undefined,
-  routes: [
-  {
-    "renderMode": 2,
-    "route": "/ProfessionalITWebSiteAngular"
-  }
-],
-  entryPointToBrowserMapping: undefined,
+  routes: undefined,
+  entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 483, hash: '0fac9c47901095062542663514c740cffd65dbcec9efbf199302112c8c726d73', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 996, hash: '623503f0922ea263a4988f58d43944b33d354233076952a84b012d11860c454a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 21634, hash: '02c7482ba5d903cbb69a07491973bf2a1f01ba4d71fe79a9015773f6510acd49', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 542, hash: '61f3fb6a42d3ba7f1b0d4e0ab2b4662722cd46b9f3f6fdab464bd07834fc2b4d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1055, hash: '11cf23656a3f48c301f997396dfc30cfefe5a5a8731c5db9339be6ff503b602a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -9,6 +9,6 @@ export default `<!doctype html>
 <link rel="stylesheet" href="styles-5INURTSO.css"></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="main-2XMEO3T5.js" type="module"></script></body>
+<script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-YCUNS2EO.js" type="module"></script></body>
 </html>
 `;

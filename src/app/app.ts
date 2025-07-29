e49b883fa7f0bfdf576d0015797597b3af22f178
manish-myteaching <manishverma.myteaching@gmail.com>
2025-07-29 12:11:36 +0530

    angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('professionait-website-workspace');
+  protected readonly title = signal('ProfessionalITWebSiteAngular');
 }
